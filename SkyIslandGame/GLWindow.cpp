96115931b490e3ecbc2cf3c49ad8d40b0f66af4c
Brandon Townsend <btownsen@dapperhat.net>
2013-01-27 13:26:11 -0800

    callback stuff

--- SkyIslandGame/GLWindow.cpp
+++ SkyIslandGame/GLWindow.cpp
@@ -1,4 +1,36 @@
 #include "GLWindow.h"
+#include <functional>
+#include <vector>
+
+#include "singleton.h"
+
+typedef std::function< void GLFWCALL(int, int)> GLFWKeyCallback;
+
+class CallbackForwarder : public Singleton<CallbackForwarder>
+{
+	std::vector<GLFWKeyCallback> callbacks;
+public:
+	void forward(int arg1, int arg2)
+	{
+		for (auto & callback : callbacks)
+			callback(arg1, arg2);
+
+	}
+	void registerCallback(GLFWKeyCallback callback)
+	{
+		callbacks.push_back(callback);
+	}
+};
+
+void forwardCallbacks(int arg1, int arg2)
+{
+	CallbackForwarder::instance().forward(arg1, arg2); 
+}
+
+void registerCallback(GLFWKeyCallback callback)
+{
+	CallbackForwarder::instance().registerCallback(callback);
+}
 
 
 int GLWindow::init(char *windowTitle)
@@ -23,18 +55,23 @@ int GLWindow::open(int width, int height, bool fullscreen)
 	//register callbacks and set window title
 	if(returnVal == GL_TRUE)
 	{
-		glfwSetWindowTitle(windowTitle);		
+		glfwSetWindowTitle(windowTitle);	
 
-		glfwSetCharCallback(GLFW_CALLBACKS::charCallback);
-		glfwSetKeyCallback(GLFW_CALLBACKS::keyCallback);
-		glfwSetMouseButtonCallback(GLFW_CALLBACKS::mouseButtonCallback);
-		glfwSetMousePosCallback(GLFW_CALLBACKS::mousePosCallback);
-		glfwSetMouseWheelCallback(GLFW_CALLBACKS::mouseWheelCallback);
+		registerCallback([&](int a, int b){this->keyCallback(a, b);});
+		glfwSetKeyCallback(&forwardCallbacks);
 
-		glfwSetWindowCloseCallback(GLFW_CALLBACKS::windowCloseCallback);
-		glfwSetWindowSizeCallback(GLFW_CALLBACKS::windowSizeCallback);
-		glfwSetWindowRefreshCallback(GLFW_CALLBACKS::windowRefreshCallback);	
-	
+
+
+		/*glfwSetCharCallback(this->charCallback);
+		
+		glfwSetMouseButtonCallback(this->mouseButtonCallback);
+		glfwSetMousePosCallback(this->mousePosCallback);
+		glfwSetMouseWheelCallback(this->mouseWheelCallback);
+
+		glfwSetWindowCloseCallback(this->windowCloseCallback);
+		glfwSetWindowSizeCallback(this->windowSizeCallback);
+		glfwSetWindowRefreshCallback(this->windowRefreshCallback);	
+	*/
 	}
 
 	return returnVal;
@@ -54,14 +91,13 @@ void GLWindow::close()
 }
 
 //callbacks
-namespace GLFW_CALLBACKS
-{
-	int GLFWCALL windowCloseCallback(){return GL_TRUE;}
-	void GLFWCALL windowSizeCallback(int width, int height){}
-	void GLFWCALL windowRefreshCallback(){}
-	void GLFWCALL keyCallback(int key, int action){}
-	void GLFWCALL charCallback(int character, int action){}
-	void GLFWCALL mouseButtonCallback(int button, int action){}
-	void GLFWCALL mousePosCallback(int x, int y){}
-	void GLFWCALL mouseWheelCallback(int pos){}
-}
+
+int GLFWCALL GLWindow::windowCloseCallback(){return GL_TRUE;}
+void GLFWCALL GLWindow::windowSizeCallback(int width, int height){}
+void GLFWCALL GLWindow::windowRefreshCallback(){}
+void GLFWCALL GLWindow::keyCallback(int key, int action){}
+void GLFWCALL GLWindow::charCallback(int character, int action){}
+void GLFWCALL GLWindow::mouseButtonCallback(int button, int action){}
+void GLFWCALL GLWindow::mousePosCallback(int x, int y){}
+void GLFWCALL GLWindow::mouseWheelCallback(int pos){}
+
--- SkyIslandGame/GLWindow.h
+++ SkyIslandGame/GLWindow.h
@@ -3,7 +3,6 @@
 #include "singleton.h"
 #include <GL/glfw.h>
 
-#include <queue>
 
 class GLWindow : public Singleton<GLWindow>
 {
@@ -11,11 +10,6 @@ class GLWindow : public Singleton<GLWindow>
 	bool fullscreen;
 	int width, height;
 
-	std::queue<int[2]> keyEvents;
-	std::queue<int[2]> charEvents;
-	std::queue<int[2]> mousePosEvents;
-	std::queue<int[2]> mouseButtonEvents;
-	std::queue<int> mouseWheelEvents;
 public:
 	int init(char *windowTitle);
 	int open(int width, int height, bool fullscreen);
@@ -23,18 +17,14 @@ public:
 	void close();
 	bool isOpen();
 	void swapBuffers();
-	
-};
 
-namespace GLFW_CALLBACKS
-{
+	void GLFWCALL keyCallback(int key, int action);
 	int GLFWCALL windowCloseCallback();
 	void GLFWCALL windowSizeCallback(int width, int height);
 	void GLFWCALL windowRefreshCallback();
-	void GLFWCALL keyCallback(int key, int action);
 	void GLFWCALL charCallback(int character, int action);
 	void GLFWCALL mouseButtonCallback(int button, int action);
 	void GLFWCALL mousePosCallback(int x, int y);
 	void GLFWCALL mouseWheelCallback(int pos);
-}
-
+	
+};
