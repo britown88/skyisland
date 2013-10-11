1add3a9ab6cfd11176e499c2c40adb216d611d2d
Brandon Townsend <btownsen@dapperhat.net>
2013-10-10 17:42:01 -0700

    proper velocity capping

--- SkyIsland/Physics.cpp
+++ SkyIsland/Physics.cpp
@@ -34,24 +34,17 @@ void Physics::updateWorldPhsyics(IScene &world, Rectf viewBounds)
                   cos(a.direction * PI / 180.0f) * a.acceleration, 
                   -(sin(a.direction * PI / 180.0f) * a.acceleration));
 
-               //v.velocity = v.velocity + delta;
+               v.velocity += delta * dt;
 
-               if(fabs(v.velocity.x + delta.x * dt) <= a.maxVelocity) 
-                  v.velocity.x += delta.x * dt;
-               else
-                  if(v.velocity.x > 0.0f)
-                     v.velocity.x = a.maxVelocity;
-                  else
-                     v.velocity.x = -a.maxVelocity;
-               
-               if(fabs(v.velocity.y + delta.y * dt) <= a.maxVelocity) 
-                  v.velocity.y += delta.y * dt;   
-               else
-                  if(v.velocity.y > 0.0f)
-                     v.velocity.y = a.maxVelocity;
-                  else
-                     v.velocity.y = -a.maxVelocity;
+               //cap
+               float mag = sqrt(v.velocity.x*v.velocity.x + v.velocity.y*v.velocity.y);
 
+               if(fabs(mag) > a.maxVelocity)
+               {
+                  float angle = atan2(v.velocity.y, v.velocity.x);
+                  v.velocity.x = cos(angle) * a.maxVelocity;
+                  v.velocity.y = sin(angle) * a.maxVelocity;
+               }
             }
 
             if(ent.hasComponent<FrictionComponent>())
